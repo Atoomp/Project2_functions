ce82274cbf552b7647d7b15b0d65db01558fab0b
Atoomp <86169325+Atoomp@users.noreply.github.com>
2022-03-20 03:45:55 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function myFunction() {                   //Defining a function and naming it
+    var sentence = "I am learning";       //Defining a variable and giving it
+    sentence += "a lot from this book!";  //a string value
+    document.getElementById("Concatenate").innerHTML = sentence;  //Putting the value
+                                          //of the variable into the HTML elementFrompoint
+                                         //with the "Concatenate" id
+
+}                                   
